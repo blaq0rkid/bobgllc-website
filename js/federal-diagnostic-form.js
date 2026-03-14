f6990179df82c02b684d97099314515ebc3e9599
blaq0rkid <blaq0rkid@users.noreply.github.com>
2026-03-14 09:00:50 -0700

    Delete js/federal-diagnostic-form.js

--- js/federal-diagnostic-form.js
+++
@@ -1,6 +0,0 @@
-// Federal Diagnostic Form Handler
-document.getElementById('federalDiagnosticForm')?.addEventListener('submit', function(e) {
-    e.preventDefault();
-    showThankYouModal();
-    this.reset();
-});
